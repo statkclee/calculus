2d4e5003da53e28f355f0517c2ac5d7c1655084c
Kwangchun Lee <statkclee@kaist.ac.kr>
2020-08-30 13:35:53 +0900

    Start Calculus Journey with R Package

---
+++ R/add_two.R
@@ -0,0 +1,9 @@
+#' 숫자 두개를 더해서 결과를 반환시키는 프로그램
+#' Add two numbers and retun added summation
+#' @encoding UTF-8
+#' @export
+#' @param a numeric scalar
+#' @param b numeric scalar
+add_two <- function(a, b) {
+  a + b
+}
---
+++ R/hello.R
@@ -0,0 +1,6 @@
+#' Hello World 환영합니다.
+#' @encoding UTF-8
+
+hello <- function() {
+  print(stringi::stri_escape_unicode("Hello, world! Tidyverse Korea"))
+}
